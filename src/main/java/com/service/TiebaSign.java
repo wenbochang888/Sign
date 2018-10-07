ce552650c89d13052e40f7c11920cdb605b66ab1
wenbochang888 <727987105@qq.com>
2018-10-07 19:42:04 +0800

    删除静态资源，改用CDN

--- src/main/java/com/service/TiebaSign.java
+++ src/main/java/com/service/TiebaSign.java
@@ -33,6 +33,7 @@ public class TiebaSign implements Runnable {
 	}
 
 	public String getCookie(String username) {
+
 		return dao.select(username);
 	}
 
@@ -74,7 +75,7 @@ public class TiebaSign implements Runnable {
 			if (flag) {
 				// 插入到数据库中
 				String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
-				dao.insertRecord(username, title, date, "签到失败");
+				dao.insertRecord(username, title, date, "已签到");
 			}
 		}
 	}
--- src/main/java/com/web/SignController.java
+++ src/main/java/com/web/SignController.java
@@ -5,19 +5,23 @@ import com.service.CookieCheckService;
 import com.service.TiebaSign;
 import com.service.UseCheck;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: Chang
  * @Date: 2018/10/3
  */
-@RestController
+@CrossOrigin
+@Controller
 public class SignController {
 
 	@Autowired
@@ -29,25 +33,34 @@ public class SignController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello World";
+
+		return "login";
 	}
 
 	@RequestMapping(value = "/check", method = RequestMethod.POST)
 	public String check(@RequestParam("cookie") String cookie,
-	                    @RequestParam("username")String username) {
+	                    @RequestParam("username") String username,
+	                    Map<String, Object> map) {
 		if (check.checkUseName(username) != null) {
-			return "用户名输入重复，请重新输入";
+			map.put("msg", "用户名输入重复，请重新输入");
+			return "/login";
 		}
 		boolean flag = cookieCheckService.check(cookie, username);
 		if (flag) {
-			return "cookie有效，登录成功";
+			map.put("msg", "cookie有效，登录成功");
+			sign.setUsername(username);
+			Thread t = new Thread(sign);
+			t.start();
+			return "redirect:/" + username;
 		} else {
-			return "请检查，cookie错误，登录失败";
+			map.put("msg", "请检查，cookie错误，登录失败");
+			return "/login";
 		}
 	}
 
+	@ResponseBody
 	@RequestMapping(value = "/sign", method = RequestMethod.POST)
-	public String sign(@RequestParam("username")String username) {
+	public String sign(@RequestParam("username") String username) {
 		if (check.checkUseName(username) == null) {
 			return "请检查用户名是否有误，请重新输入";
 		}
@@ -58,8 +71,10 @@ public class SignController {
 	}
 
 	@RequestMapping(value = "/{username}", method = RequestMethod.GET)
-	public List<Record> selectRecord(@PathVariable(value = "username") String username) {
+	public String selectRecord(@PathVariable(value = "username") String username,
+	                           Map<String, Object> map) {
 		List<Record> res = check.selectRecord(username);
-		return res;
+		map.put("res", res);
+		return "/info";
 	}
 }
