a8fa8a0b60a5d9b47cb6b54ff4f53cc8b448b13c
wenbochang888 <727987105@qq.com>
2018-10-31 10:54:38 +0800

    测试创建分支

--- src/main/java/com/SignApplication.java
+++ src/main/java/com/SignApplication.java
@@ -2,7 +2,6 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,8 +14,8 @@ public class SignApplication extends SpringBootServletInitializer {
 		SpringApplication.run(SignApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SignApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(SignApplication.class);
+//	}
 }
