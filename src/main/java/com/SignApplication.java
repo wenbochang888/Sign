0b57f43d05468bd2c45895657f32fcb95157c530
wenbochang888 <727987105@qq.com>
2018-11-19 14:29:01 +0800

    修改main

--- src/main/java/com/SignApplication.java
+++ src/main/java/com/SignApplication.java
@@ -2,21 +2,13 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class SignApplication extends SpringBootServletInitializer {
+public class SignApplication {
 
 	public static void main(String[] args) {
-		String ss = "ss";
-		System.out.println(ss);
 		SpringApplication.run(SignApplication.class, args);
 	}
-
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		return builder.sources(SignApplication.class);
-//	}
 }
